15853293a6209466b6bda129118546c48e9faa3a
Suryam Jain <suryam@Suryams-MacBook-Pro.local>
2022-01-07 00:04:11 +0530

    SB project

---
+++ Java/training/src/main/java/com/consultadd/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.consultadd.controller;
+
+import com.consultadd.model.Employee;
+import com.consultadd.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployees(){
+        List<Employee> employeeList = employeeService.getEmployees();
+        return employeeList;
+    }
+
+    @PostMapping("/addemp")
+    public String saveEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+}
